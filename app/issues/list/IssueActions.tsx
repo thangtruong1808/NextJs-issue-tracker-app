4e36d3c99b80474a565e02f69bfe7f0865db620a
Thang-Truong <thangtruong1808@gmail.com>
2024-04-29 20:34:08 +1000

    add Select filter component

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,18 +1,20 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 
 import { MdAdd } from "react-icons/md";
+import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueActions = () => {
   return (
     <>
-      <div className="mb-5">
+      <Flex mb="5" justify="between">
+        <IssueStatusFilter />
         <Button>
           {/* <GrNew /> */}
           <Link href={"/issues/new"}>New Issue</Link>
           <MdAdd size={20} />
         </Button>
-      </div>
+      </Flex>
     </>
   );
 };
